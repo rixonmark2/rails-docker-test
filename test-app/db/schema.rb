610d019d5c502d8072ed7985788b5c69c74597c3
rixonmark2 <ne240163@gmail.com>
2022-02-24 22:35:36 +0900

    ・リスト6.26まで

--- test-app/db/schema.rb
+++ test-app/db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_13_060334) do
+ActiveRecord::Schema.define(version: 2022_02_16_131554) do
+
+  create_table "events", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "owner_id"
+    t.string "name", null: false
+    t.string "place", null: false
+    t.datetime "start_at", null: false
+    t.datetime "end_at", null: false
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["owner_id"], name: "index_events_on_owner_id"
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "provider", null: false
